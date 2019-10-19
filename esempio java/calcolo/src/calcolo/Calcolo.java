3d3b050e557d5acdccd964f745b07820edbb0637
Francesco-Sarra-Peano-4A <56753334+Francesco-Sarra-Peano-4A@users.noreply.github.com>
2019-10-19 10:35:36 +0200

    Add files via upload

---
+++ esempio java/calcolo/src/calcolo/Calcolo.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcolo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sarra.francesco
+ */
+public class Calcolo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int b, h, area, perimetro;
+        Scanner tastiera = new Scanner(System.in);
+        
+        System.out.println("Inserisci la base: ");
+        b = tastiera.nextInt();
+        System.out.println("Inserisci l'altezza: ");
+        h = tastiera.nextInt();
+        System.out.println("Area: " + calcolo(b, h, 'a'));
+        System.out.println("Perimetro: " + calcolo(b, h, 'p'));
+    }
+
+   public static int calcolo(int b, int h, char c) {
+      switch (c) {
+          case 'p' : return 2 *(b + h);
+          case 'a' : return b * h;
+          default : return 0;
+      }  
+    }
+    
+}
